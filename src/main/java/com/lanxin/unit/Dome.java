b218548a537ea0b070053d873afcc80f025470d8
13277112287 <1115759748@qq.com>
2020-08-14 15:54:10 +0800

    hello lizhi

--- src/main/java/com/lanxin/unit/Dome.java
+++ src/main/java/com/lanxin/unit/Dome.java
@@ -4,11 +4,10 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.realm.SimpleAccountRealm;
 import org.apache.shiro.subject.Subject;
 
 public class Dome {
-
+private String s1;
     public static void main(String[] args) {
 //        SimpleAccountRealm simpleAccountRealm=new SimpleAccountRealm();
 //
